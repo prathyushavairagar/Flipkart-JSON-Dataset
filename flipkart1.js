16a14502218e5e90e2af6a41728d4dae3c79d0ae
prathyushavairagar <prathyusha.vairagar@gmail.com>
2018-05-29 18:06:43 +0530

    Add files via upload

---
+++ flipkart1.js
@@ -0,0 +1,657 @@
+var flipkartDataset={
+	"Departments": [{
+
+			"id": "1",
+			"name": "Electronics",
+
+			"categories": [{
+
+					"num": "001",
+
+					"Mobiles": [{
+
+							"brand": "samsung",
+							"productname": "Samsung Galaxy J6",
+							"color": "black",
+							"ram": "3gb",
+							"OS": "Android",
+							"price": "Rs 13990",
+							"warranty": "12 Months",
+							"storage": "32gb",
+							"status": "Coming Soon"
+						},
+
+						{
+							"brand": "samsung",
+							"productname": "samsung galaxy J7 duo",
+							"color": "gold",
+							"ram": "4gb",
+							"OS": "Android",
+							"price": "Rs 15990",
+							"warranty": "12 Months",
+							"storage": "32gb",
+							"status": "available"
+						},
+
+						{
+							"brand": "samsung",
+							"productname": "samsung galaxy s9 plus",
+							"color": "purple",
+							"ram": "6gb",
+							"OS": "Android",
+							"price": "Rs 68000",
+							"warranty": "12 Months",
+							"storage": "128gb",
+							"status": "available"
+						},
+
+						{
+							"brand": "Apple",
+							"productname": "Iphone 8 plus",
+							"color": "space grey",
+							"price": "Rs 76580",
+							"warranty": "1 year",
+							"storage": "64gb",
+							"status": "available"
+						},
+
+						{
+							"brand": "Vivo",
+							"productname": "Vivo V5s",
+							"color": "White",
+							"price": "Rs 15590",
+							"ram": "4gb",
+							"warranty": "1 year",
+							"storage": "64gb",
+							"status": "available"
+						},
+
+						{
+							"brand": "Honor",
+							"productname": "Honor 9 lite",
+							"color": "Glacier grey",
+							"ram": "4gb",
+							"warranty": "1 year",
+							"storage": "64gb",
+							"price": "Rs 14999",
+							"status": "available"
+
+						}
+					]
+				},
+				{
+					"num": "002",
+
+					"Laptops": [{
+
+							"brand": "Lenovo",
+							"processor": "intel core i5",
+							"ram": "8gb",
+							"Hdd": "2TB",
+							"display": "15.6 inches",
+							"OS": "windows 10",
+							"warranty": "1 year",
+							"price": "46990"
+						},
+
+						{
+							"brand": "Asus",
+							"processor": "intel core i7",
+							"ram": "8gb",
+							"Hdd": "1TB",
+							"display": "15.6 inches",
+							"OS": "windows 10",
+							"warranty": "1 year",
+							"price": "57990"
+						}
+					]
+				},
+				{
+					"num": "003",
+					"Power Bank": [{
+
+							"brand": "Lenovo",
+							"productName": "Lenovo PA 10400 10400 mAh Power Bank",
+							"color": "White/Black",
+							"price": "Rs.920",
+							"warranty": "12 months",
+							"suitableFor": ["Camera", "MP3 Player", "Mobile", " Tablet"],
+							"model": "LT-AB550/IN",
+							"capacity": "10000 mAh",
+							"status": "available"
+
+						},
+
+						{
+
+							"brand": "Mi",
+							"productName": "Mi PLM05ZM 20000 mAh Power Bank",
+							"color": "White/Black",
+							"price": "Rs.999",
+							"warranty": "12 months",
+							"suitableFor": ["Camera", "MP3 Player", "Mobile", " Tablet"],
+							"model": "PLM05ZM",
+							"capacity": "12000 mAh",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Syska",
+							"productName": "Syska Power Pro 200- 20000 mAh Power Bank",
+							"color": "White/Black",
+							"price": "Rs.1120",
+							"warranty": "12 months",
+							"suitableFor": ["Camera", "MP3 Player", "Mobile", " Tablet"],
+							"model": "SPP-AB550/IN",
+							"capacity": "20000 mAh",
+							"status": "available"
+						}
+					]
+				},
+				{
+
+					"num": "004",
+					"Ear Phones": [{
+
+
+							"brand": "JBL",
+							"productName": "JBL T250SI Wired Headphone",
+							"color": "White",
+							"price": "Rs.739",
+							"warranty": "12 months",
+							"suitableFor": "Mobile",
+							"model": "T250SI",
+							"cableSpeed": "000 Mbps",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "boAt",
+							"productName": "boAt BassHeads 220 Headset with Mic",
+							"color": "Blue",
+							"price": "Rs.420",
+							"warranty": "12 months",
+							"suitableFor": ["MP3 Player", "Mobile", " Tablet"],
+							"model": "BassHeads 220",
+							"cableSpeed": "000 Mbps",
+							"status": "available"
+						},
+
+						{
+							"brand": "Sennheiser",
+							"productName": "Sennheiser CX 180 Wired Headphone",
+							"color": "Black",
+							"price": "Rs.790",
+							"warranty": "12 months",
+							"suitableFor": ["Mobile", " Tablet"],
+							"model": "CX 180",
+							"cableSpeed": "000 Mbps",
+							"status": "available"
+
+						}
+					]
+
+				}
+			]
+
+		},
+		{
+
+			"id": "2",
+			"Men": [{
+
+					"product": "T-shirts",
+
+					"sub_products": [{
+
+							"brand": "Nike",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.639",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.2",
+							"status": "available"
+
+						},
+
+						{
+
+							"brand": "Adidas",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.839",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.5",
+							"status": "available"
+
+						},
+
+						{
+
+							"brand": "Puma",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.739",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.8",
+							"status": "available"
+
+						}
+					]
+				},
+				{
+					"product": "Shirts",
+
+					"sub_products": [{
+
+							"brand": "Nike",
+							"colors": ["White/Red", "Black/Green"],
+							"price": "Rs.639",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Adidas",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.839",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.5",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Puma",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.739",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.8",
+							"status": "available"
+						}
+					]
+				},
+				{
+					"product": "Trousers",
+
+					"sub_products": [{
+
+							"brand": "United Colors of Benetton",
+							"colors": ["White", "Black"],
+							"price": "Rs.939",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+							"brand": "Lee",
+							"colors": ["White", "Black"],
+							"price": "Rs.1039",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+							"brand": "John Players",
+							"colors": ["White", "Black"],
+							"price": "Rs.939",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						}
+					]
+				},
+				{
+					"product": "Shorts",
+
+					"sub_products": [{
+
+							"brand": "Lee",
+							"colors": ["White", "Black"],
+							"price": "Rs.939",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Nike",
+							"colors": ["White", "Black"],
+							"price": "Rs.439",
+							"idealFor": "Men",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "John Players",
+							"colors": ["White", "Black"],
+							"price": "Rs.699",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						}
+					]
+				},
+				{
+					"product": "Jeans",
+
+					"sub_products": [{
+
+							"brand": "Lee",
+							"colors": ["White", "Black"],
+							"price": "Rs.1039",
+							"idealFor": "Men",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Nike",
+							"colors": ["White", "Black"],
+							"price": "Rs.1439",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Jack & Jones",
+							"colors": ["White", "Black"],
+							"price": "Rs.1699",
+							"size": [32, 34, 36, 38],
+							"userRating": "4.2",
+							"status": "coming soon"
+
+						}
+					]
+
+				}
+			]
+		},
+		{
+
+			"id": "3",
+			"Women": [{
+
+					"product": "T-shirts",
+
+					"sub_products": [{
+
+							"brand": "Forever21",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.639",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.2",
+							"status": "available"
+
+						},
+
+						{
+
+							"brand": "People",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.839",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.5",
+							"status": "available"
+
+						},
+
+						{
+
+							"brand": "Vero Moda",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.739",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.8",
+							"status": "available"
+
+						}
+					]
+				},
+				{
+					"product": "Shirts",
+
+					"sub_products": [{
+
+							"brand": "United Colors of Benetton",
+							"colors": ["White/Red", "Black/Green"],
+							"price": "Rs.639",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Peter England",
+							"colors": ["White", "Red", "Black", "Purple"],
+							"price": "Rs.839",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.5",
+							"status": "available"
+						}
+					]
+
+				},
+				{
+					"product": "Kurtis",
+
+					"sub_products": [{
+
+							"brand": "Aurelia",
+							"colors": ["White/Red", "Black/Green"],
+							"price": "Rs.639",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "W",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.839",
+							"size": ["S", "M", "XL", "XXL"],
+							"userRating": "4.5",
+							"status": "available"
+						}
+					]
+
+				},
+				{
+					"product": "Sarees",
+
+					"sub_products": [{
+
+							"brand": "Banarasi Silk",
+							"colors": ["White", "Red", "Black", "Green"],
+							"price": "Rs.800",
+							"userRating": "4.2",
+							"status": "available"
+						},
+
+						{
+
+							"brand": "Crape",
+							"colors": ["White", "Red", "Black", "Pink"],
+							"price": "Rs.400",
+							"userRating": "4.5",
+							"status": "available"
+
+						}
+					]
+
+
+				}
+
+			]
+		},
+		{
+			"num": "4",
+			"Furniture": [{
+
+				"product": "Chair",
+				"sub_products": [{
+
+						"brand": "Cello Furniture",
+						"productName": "Cello Furniture Plastic Living Room Chair",
+						"price": "Rs.4091",
+						"idealFor": "Living Room ",
+						"material": ["Fabric", "Plastic", "Metal"],
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					},
+
+					{
+						"brand": "Ks chairs Fabric",
+						"productName": "Ks chairs Fabric Office Arm Chair",
+						"price": "Rs.3091",
+						"idealFor": "Office",
+						"material": "plastic",
+						"userRating": "4.2",
+						"color": "Brown",
+						"status": "available"
+					},
+
+					{
+						"brand": "Regentseating",
+						"productName": "Regentseating Fabric Office Arm Chair ",
+						"price": "Rs.6491",
+						"idealFor": "Office",
+						"material": ["Fabric", "Plastic", "Metal"],
+						"userRating": "4.2",
+						"color": "Brown",
+						"status": "available"
+					}
+				]
+			}, {
+				"product": "Tables",
+				"sub_products": [{
+
+						"brand": "Cello Furniture",
+						"productName": "Cello Furniture Dining Table",
+						"price": "Rs.14091",
+						"idealFor": "Dining",
+						"material": ["Glass", "Metal"],
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					},
+
+					{
+						"brand": "Induscraft",
+						"productName": "Induscraft Solid Wood 4 Seater Dining Set",
+						"price": "Rs.3091",
+						"idealFor": "Dining",
+						"material": "Wood",
+						"userRating": "4.2",
+						"color": "Brown",
+						"status": "available"
+					},
+
+					{
+						"brand": "Regentseating",
+						"productName": "Regentseating Office Table",
+						"price": "Rs.6491",
+						"idealFor": "Office",
+						"material": "Wood",
+						"userRating": "4.2",
+						"color": "Brown",
+						"status": "available"
+					}
+				]
+			}, {
+				"product": "Beds",
+				"sub_products": [{
+
+						"brand": "HomeTown Bali Bolton",
+						"productName": "HomeTown Bali Bolton Engineered Wood Queen Bed",
+						"price": "Rs.14091",
+						"material": "Saag Wood",
+						"size": "Queen",
+						"withMattress": true,
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					},
+
+					{
+						"brand": "Auspicious",
+						"productName": "Auspicious Home Iceland Engineered Wood Queen Bed",
+						"price": "Rs.15091",
+						"material": "Saag Wood",
+						"size": "Queen",
+						"withMattress": true,
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					},
+
+					{
+						"brand": "HomeTown Bali Bolton",
+						"productName": "HomeTown Bali Bolton Engineered Wood King Bed",
+						"price": "Rs.34091",
+						"material": "Saag Wood",
+						"size": "King",
+						"withMattress": true,
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					}
+				]
+			}, {
+
+				"product": "Sofa",
+				"sub_products": [{
+
+						"brand": "Kurlon",
+						"productName": "Kurlon Bullet Leatherette 1 Seater Sofa",
+						"price": "Rs.4091",
+						"material": "Saag Wood",
+						"size": "Medium",
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					},
+
+					{
+						"brand": "Urban Living ",
+						"productName": "Urban Living Derby Fabric 1 Seater Standard",
+						"price": "Rs.5091",
+						"material": "Saag Wood",
+						"size": "Medium",
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					},
+
+					{
+						"brand": "Kurlon",
+						"productName": "Kurlon Dazzle Fabric 3 Seater Sofa",
+						"price": "Rs.24091",
+						"material": "Saag Wood",
+						"size": "Large",
+						"userRating": "4.2",
+						"color": "Black",
+						"status": "available"
+					}
+
+				]
+			}]
+
+		}
+	]
+}
+alert(flipkartDataset.Departments[0].name)
